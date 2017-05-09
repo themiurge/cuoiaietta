e158ad1d83fb2ed8ba38efdf2adfdaed6d010a4b
Emilio Vicari <emilio.vicari@gmail.com>
2017-05-09 21:55:59 +0200

    added syllables project

---
+++ Syllables/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Syllables
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtStandard = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(12, 12);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(555, 20);
+            this.txtSource.TabIndex = 1;
+            this.txtSource.TextChanged += new System.EventHandler(this.txtSource_TextChanged);
+            // 
+            // txtStandard
+            // 
+            this.txtStandard.Enabled = false;
+            this.txtStandard.Location = new System.Drawing.Point(12, 38);
+            this.txtStandard.Name = "txtStandard";
+            this.txtStandard.Size = new System.Drawing.Size(555, 20);
+            this.txtStandard.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 67);
+            this.Controls.Add(this.txtStandard);
+            this.Controls.Add(this.txtSource);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Scrivi!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtStandard;
+    }
+}
+
---
+++ Syllables/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Syllables
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        enum enumGroupType { VOWEL, CONSONANT, NONE }
+
+        static char cleanChar(char c)
+        {
+            c = char.ToLower(c);
+            switch (c)
+            {
+                case 'è':
+                case 'é':
+                    c = 'e';
+                    break;
+                case 'à':
+                    c = 'a';
+                    break;
+                case 'ì':
+                    c = 'i';
+                    break;
+                case 'ù':
+                    c = 'u';
+                    break;
+                case 'ò':
+                    c = 'o';
+                    break;
+                case 'ç':
+                    c = 'c';
+                    break;
+                case '\'':
+                case '-':
+                    return '\0';
+            }
+            return c;
+        }
+
+        static List<string> TRIPHTONGS = new List<string>() { "iai", "iei", "uoi", "uai", "uei", "iuo", "ioi", "iui" };
+
+        class CharGroup
+        {
+            public CharGroup(string s, enumGroupType type)
+            {
+                this.repr = s;
+                this.type = type;
+            }
+            public string repr;
+            public enumGroupType type;
+            public List<CharGroup> split()
+            {
+                List<CharGroup> groupsToAdd = new List<CharGroup>();
+                Stack<CharGroup> toAddStacked = new Stack<CharGroup>();
+                if (repr.Length == 1) return groupsToAdd;
+                bool split = false;
+                if (type == enumGroupType.CONSONANT)
+                {
+                    if (repr.Length >= 3)
+                        split = repr[0] != 's';
+                    else
+                        split = toBeSeparated(repr[0], repr[1]);
+                }
+                else
+                {
+                    while (repr.Length > 3)
+                    {
+
+                        int lenToRemove = 1;
+
+                        if (TRIPHTONGS.Contains(repr.Substring(repr.Length - 3)))
+                            lenToRemove = 3;
+                        else if (isDiphtong(repr[repr.Length - 2], repr[repr.Length - 1]))
+                            lenToRemove = 2;
+
+                        toAddStacked.Push(new CharGroup(repr.Substring(repr.Length - lenToRemove), enumGroupType.VOWEL));
+                        repr = repr.Substring(0, repr.Length - lenToRemove);
+
+                    }
+                    if (repr.Length == 3)
+                        split = !TRIPHTONGS.Contains(repr);
+                    else if (repr.Length == 2)
+                        split = !isDiphtong(repr[0], repr[1]);
+                }
+                if (split)
+                {
+                    CharGroup ret = new CharGroup(repr.Substring(1), this.type);
+                    repr = repr[0].ToString();
+                    groupsToAdd.Add(ret);
+                }
+                while (toAddStacked.Count > 0)
+                    groupsToAdd.Add(toAddStacked.Pop());
+                return groupsToAdd;
+            }
+        }
+
+        static enumGroupType getTypeFromChar(char c, char? before = null, char? after = null)
+        {
+            if (c == 'i' && before.HasValue && (before == 'c' || before == 'g') && after.HasValue && getTypeFromChar(after.Value) == enumGroupType.VOWEL)
+                return enumGroupType.CONSONANT;
+            return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') ? enumGroupType.VOWEL : enumGroupType.CONSONANT;
+        }
+
+        static bool isDiphtong(char a, char b)
+        {
+            switch (a)
+            {
+                case 'i':
+                case 'u':
+                    return true;
+                default:
+                    switch (b)
+                    {
+                        case 'i':
+                            return true;
+                        case 'u':
+                            return a == 'a' || a == 'e';
+                    }
+                    return false;
+            }
+        }
+
+        const string INSEP_CONS_PRE = "bcdfgptv";
+        const string INSEP_CONS_POST = "lr";
+
+        static bool toBeSeparated(char a, char b)
+        {
+            if (a == b) return true;
+            switch (a)
+            {
+                case 's':
+                    return false;
+                case 'c':
+                case 'g':
+                    if (b == 'h' || b == 'i') return false;
+                    return !INSEP_CONS_POST.Contains(b);
+            }
+            return !(INSEP_CONS_PRE.Contains(a) && INSEP_CONS_POST.Contains(b));
+        }
+
+        static string cleanString(string s)
+        {
+            string ret = "";
+            foreach (char c in s)
+            {
+                char cur = cleanChar(c);
+                if (cur != '\0')
+                    ret += cur;
+            }
+            return ret;
+        }
+
+        private List<CharGroup> getCharGroups(string word)
+        {
+            List<CharGroup> temp = new List<CharGroup>();
+            enumGroupType curType = enumGroupType.NONE;
+            CharGroup curGroup = null;
+            int i = 0;
+            char? prev = null;
+            char? next = null;
+
+
+            for (i = 0; i < word.Length; i++)
+            {
+                char c = word[i];
+                next = (i < word.Length - 1) ? (char?)word[i + 1] : null;
+                enumGroupType t = getTypeFromChar(c, prev, next);
+                if (t != curType)
+                {
+                    curGroup = new CharGroup(c.ToString(), t);
+                    curType = t;
+                    temp.Add(curGroup);
+                }
+                else
+                    curGroup.repr += c;
+                prev = c;
+            }
+
+            List<CharGroup> ret = new List<CharGroup>() { new CharGroup("", enumGroupType.NONE) };
+
+            foreach (var group in temp)
+            {
+                ret.Add(group);
+                var groupsToAdd = group.split();
+                foreach (var toAdd in groupsToAdd) ret.Add(toAdd);
+            }
+
+            ret.Add(new CharGroup("", enumGroupType.NONE));
+
+            temp = new List<CharGroup>();
+
+            i = 1;
+
+            while (i < ret.Count - 1)
+            {
+                CharGroup g = new CharGroup(ret[i].repr, ret[i].type);
+
+                while (g.type == enumGroupType.CONSONANT)
+                {
+                    g.repr += ret[++i].repr;
+                    if (ret[i].type != enumGroupType.CONSONANT)
+                        g.type = ret[i].type;
+                }
+
+                if (ret[i].type == enumGroupType.VOWEL)
+                {
+                    if (ret[i + 1].type == enumGroupType.CONSONANT && ret[i + 2].type != enumGroupType.VOWEL)
+                    {
+                        g.repr += ret[++i].repr;
+                        if (ret[i + 1].type == enumGroupType.CONSONANT && ret[i + 2].type == enumGroupType.NONE)
+                            g.repr += ret[++i].repr;
+                    }
+                }
+
+                i++;
+                temp.Add(g);
+            }
+
+            return temp;
+        }
+
+        private string getSyllables(string word)
+        {
+
+            word = cleanString(word);
+
+            List<CharGroup> groups = getCharGroups(word);
+
+            string ret = "";
+            foreach (var group in groups)
+            {
+                if (ret != "") ret += '-';
+                ret += group.repr;
+            }
+
+            return ret;
+
+        }
+
+        private void txtSource_TextChanged(object sender, EventArgs e)
+        {
+            string[] words = txtSource.Text.Split(' ').Select(s => s.Trim()).ToArray();
+            txtStandard.Text = "";
+            foreach (var word in words)
+            {
+                if (txtStandard.Text != "") txtStandard.Text += " ";
+                txtStandard.Text += getSyllables(word);
+
+            }
+        }
+    }
+}
